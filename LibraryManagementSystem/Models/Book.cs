98e70b2a60dea0734bf786a2ccc9b5ba8758a177
MaksItLab <maksim.ardintsev@mail.ru>
2025-07-18 13:39:07 +0300

    Добавьте файлы проекта.

---
+++ LibraryManagementSystem/Models/Book.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryManagementSystem.Models
+{
+    /// <summary>
+    /// Представляет книгу в библиотеке.
+    /// Содержит информацию о названии, авторе, ISBN, годе издания и доступности.
+    /// </summary>
+    public class Book
+    {
+        /// <summary>
+        /// Название книги.
+        /// </summary>
+        public string Title { get; private set; }
+
+        /// <summary>
+        /// Автор книги.
+        /// </summary>
+        public string Author { get; private set; }
+
+        /// <summary>
+        /// Уникальный идентификатор книги (например, ISBN).
+        /// </summary>
+        public string ISBN { get; private set; }
+
+        /// <summary>
+        /// Год издания книги.
+        /// </summary>
+        public int Year { get; private set; }
+
+        /// <summary>
+        /// Доступна ли книга для выдачи.
+        /// </summary>
+        public bool IsAvailable { get; private set; }
+
+        /// <summary>
+        /// Создаёт новую книгу и помечает её как доступную.
+        /// </summary>
+        public Book(string title, string author, string isbn, int year)
+        {
+            Title = title;
+            Author = author;
+            ISBN = isbn;
+            Year = year;
+            IsAvailable = true;
+        }
+
+        /// <summary>
+        /// Помечает книгу как выданную (становится недоступной).
+        /// </summary>
+        public void MarkAsLent()
+        {
+            IsAvailable = false;
+        }
+
+        /// <summary>
+        /// Помечает книгу как возвращённую (становится доступной).
+        /// </summary>
+        public void MarkAsReturned()
+        {
+            IsAvailable = true;
+        }
+    }
+
+}
---
+++ LibraryManagementSystem/Models/User.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryManagementSystem.Models
+{
+    /// <summary>
+    /// Абстрактный пользователь системы.
+    /// Имеет уникальный идентификатор, имя и роль.
+    /// Наследуется в Reader и Librarian.
+    /// </summary>
+    public abstract class User
+    {
+        /// <summary>
+        /// Уникальный идентификатор пользователя.
+        /// </summary>
+        public string Id { get; }
+
+        /// <summary>
+        /// Имя пользователя.
+        /// </summary>
+        public string Name { get; }
+
+        /// <summary>
+        /// Роль пользователя (например, Reader или Librarian).
+        /// </summary>
+        public string Role { get; protected set; }
+
+        /// <summary>
+        /// Создаёт нового пользователя с уникальным идентификатором.
+        /// </summary>
+        protected User(string name)
+        {
+            Id = Guid.NewGuid().ToString();
+            Name = name;
+        }
+
+        /// <summary>
+        /// Абстрактный метод, выводящий информацию о правах пользователя.
+        /// Должен быть реализован в наследниках.
+        /// </summary>
+        public abstract void ShowPermissions();
+    }
+}
