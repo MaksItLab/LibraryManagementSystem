59e39fc80e48156b1c1fe6133e4b9cde94c8aa96
Anno-45 <nikita20061108@mail.ru>
2025-07-28 17:51:06 +0300

    промежуточный результат реализации issue 3

---
+++ LibraryManagementSystem/Interfaces/IJsonStorage.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryManagementSystem.Interfaces
+{
+    public interface IJsonStorage
+    {
+        void Save(List<Models.Book> data, string filePath);
+        List<Models.Book> Load(string filePath);
+    }
+}
---
+++ LibraryManagementSystem/Services/JsonStorage.cs
@@ -0,0 +1,33 @@
+﻿using LibraryManagementSystem.Interfaces;
+using LibraryManagementSystem.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace LibraryManagementSystem.Services
+{
+    public class JsonStorage : IJsonStorage
+    {
+        public List<Book> Load(string filePath)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Save(List<Book> data, string filePath)
+        {
+            
+            for (int i = 0; i < data.Count; i++) 
+            {
+                //string jsonString = JsonSerializer.Serialize();
+                //File.WriteAllText("books.json", jsonString);
+            }
+        }
+    }
+}
